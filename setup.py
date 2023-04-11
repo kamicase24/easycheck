933ccffc3a994f8d977078243a9828ff4651deb6
Jesus Rojas <jrojas@doiteasy.pe>
2023-04-11 14:56:55 -0500

    [ADD] primera version

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from distutils.core import setup
 setup(
   name = 'Easycheck',         # How you named your package folder (MyLib)
   packages = ['Easycheck'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Private SDK to connect to the Easycheck Monitor',   # Give a short description about your library
   author = 'Jesus Rojas',                   # Type in your name
   author_email = 'jrojas@doiteasy.pe',      # Type in your E-Mail
   url = 'https://github.com/kamicase24',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kamicase24/easycheck/archive/refs/tags/alpha.tar.gz',    # I explain this later on
   keywords = ['MONITOR', 'SDK', 'API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests',
+          'requests',   
           'os'
       ],
   classifiers=[
