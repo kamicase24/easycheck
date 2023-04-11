220b5bea787c719b34fce283f52918bee173f0e8
Jesus Rojas <jrojas@doiteasy.pe>
2023-04-11 15:01:22 -0500

    .

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(
-  name = 'Easycheck',         # How you named your package folder (MyLib)
-  packages = ['Easycheck'],   # Chose the same as "name"
+  name = 'easycheck',         # How you named your package folder (MyLib)
+  packages = ['easycheck'],   # Chose the same as "name"
   version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Private SDK to connect to the Easycheck Monitor',   # Give a short description about your library
