81bcbd804c8912f88de059cf447f76b0024235e4
Jesus Rojas <jrojas@doiteasy.pe>
2023-04-11 17:38:58 -0500

    ...

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   author = 'Jesus Rojas',                   # Type in your name
   author_email = 'jora2415@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/kamicase24',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kamicase24/easycheck/archive/refs/tags/alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kamicase24/easycheck/archive/refs/tags/alpha-1.1.tar.gz',    # I explain this later on
   keywords = ['MONITOR', 'SDK', 'API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',   
