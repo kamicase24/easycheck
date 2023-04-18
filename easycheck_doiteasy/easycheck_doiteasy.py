a1be6f4cec897effd91d0c61cf42f601f4d9a443
Jesus Rojas <jrojas@doiteasy.pe>
2023-04-18 08:49:30 -0500

    [IMP] ver 0.0.2

--- easycheck_doiteasy/easycheck_doiteasy.py
+++ easycheck_doiteasy/easycheck_doiteasy.py
@@ -1,30 +1,58 @@
 import requests
 import os
+import logging
+logging = logging.getLogger(__name__)
 
-MONITOR_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTUsImV4cCI6MTY4NzI5NzYxNn0.k713wpFyzMc1ZgPTJjgPuJubM_stvCsQd3vb6mL-QOU'
-TEMP_DOMAIN = 'https://b0e7-38-25-12-182.sa.ngrok.io'
 
 
 class Easycheck():
 
     def __init__(self, token):
         self.token = token
+        self.domain = 'https://etlcheck.com'
 
-    def send_ejecution(data):
 
+    def send_ejecution(self, name, duration, start_datetime, end_datetime, destination, total_register, successful):
+        """
+        Sends execution data to a specified API endpoint.
+
+        Parameters:
+        - name (str): The name of the execution.
+        - duration (str): The duration of the execution in seconds.
+        - start_datetime (str): The start datetime of the execution in ISO format (e.g. '2023-04-12T10:30:00Z').
+        - end_datetime (str): The end datetime of the execution in ISO format (e.g. '2023-04-12T11:00:00Z').
+        - destination (str): The destination of the execution.
+        - total_register (int): The total number of registers processed during the execution.
+        - successful (bool): Whether the execution was successful or not.
+        Returns:
+        - None
+        Raises:
+        - This method may raise an exception if the HTTP request to the API endpoint fails for any reason. In this case, an error message will be logged.
+        """
         headers = {}
-        domain = TEMP_DOMAIN
+        domain = self.domain
         path = '/api/base/ejecution/send/'
         url = f'{domain}{path}'
-        data.update({
-            'register_token': MONITOR_TOKEN
-        })
-        res = requests.post(url, data=data, headers=headers)
-        
-    def update_logs_directories(logs_dir):
+        data = {
+            'name': name,
+            'duration': duration,
+            'start_datetime': start_datetime,
+            'end_datetime': end_datetime,
+            'destination': destination,
+            'total_register': total_register,
+            'successful': successful,
+            'register_token': self.token
+        }
+        try:
+            requests.post(url, data=data, headers=headers)
+        except Exception as e:
+            logging.error(e)
+
+
+    def update_logs_directories(self, logs_dir):
 
         headers = {}
-        domain = TEMP_DOMAIN
+        domain = self.domain
         path = '/api/base/logs_directory/register/'
         url = f'{domain}{path}'
         directories = [element for element in os.listdir(logs_dir) if os.path.isdir(f'{logs_dir}/{element}')]
@@ -33,14 +61,14 @@ class Easycheck():
         data = {
             'origin': carpeta_raiz,
             'directories': directories,
-            'register_token': MONITOR_TOKEN
+            'register_token': self.token
         }
         res = requests.post(url, data=data, headers=headers)
 
-    def register_file(log_file):
+    def register_file(self, log_file):
 
         headers = {}
-        domain = TEMP_DOMAIN
+        domain = self.domain
         path = '/api/base/logs_file/register/'
         file_size = os.path.getsize(log_file)/1024
         log_dir = log_file.split('/')[-2]
@@ -50,7 +78,7 @@ class Easycheck():
             'log_directory': log_dir,
             'file_size': file_size,
             'with_errors': False,
-            'register_token': MONITOR_TOKEN
+            'register_token': self.token
         }
         url = f'{domain}{path}'
         res = requests.post(url, data=data, headers=headers)
